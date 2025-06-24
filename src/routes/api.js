7cf844ad8a292bca741b6cfff016c65c48c6af87
Nguyen Quang Do <quangdo0206@gmail.com>
2025-06-24 22:04:04 +0700

    refactor: Remove unnecessary blank lines in API route file

--- src/routes/api.js
+++ src/routes/api.js
@@ -10,8 +10,6 @@ const routerAPI = express.Router();
 
 routerAPI.use(express.static(path.join(__dirname, 'public')));
 
-
-
 routerAPI.get('/', (req, res) => {
     return res.status(200).json('Hello world api');
 });
